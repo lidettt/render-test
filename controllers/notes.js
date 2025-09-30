b9f1709124fac9b55e9dae6470dd52eea7d088e0
Lidet Voeun <lidetvoeun@gmail.com>
2025-09-30 09:08:30 +0700

    Finish setting Project Structure

---
+++ controllers/notes.js
@@ -0,0 +1,65 @@
+const notesRouter = require("express").Router();
+const Note = require("../models/note");
+// route for getting all the notes
+notesRouter.get("/", (request, response) => {
+  Note.find({}).then((notes) => response.json(notes));
+});
+//route for fetching a single resource
+notesRouter.get("/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+notesRouter.delete("/api/notes/:id", (request, response) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+//route for adding data (new note)
+notesRouter.post("/", (request, response, next) => {
+  const body = request.body;
+  if (!body.content) {
+    return response.status(400).json({ error: "content missing" });
+  }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
+});
+
+notesRouter.put("/:id", (request, response, next) => {
+  const { content, important } = request.body;
+  Note.findById(request.params.id).then((note) => {
+    if (!note) {
+      return response.status(404).end();
+    }
+    note.content = content;
+    note.important = important;
+    return note
+      .save()
+      .then((updatedNote) => {
+        response.json(updatedNote);
+      })
+      .catch((error) => {
+        // console.log(error.response.data.error);
+        next(error);
+      });
+  });
+});
+
+module.exports = notesRouter;
