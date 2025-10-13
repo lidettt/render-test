cdf7717e5cfd75d8d85d699c5125208ef713f6e0
Lidet Voeun <lidetvoeun@gmail.com>
2025-10-13 15:29:28 +0700

    Finish Part 4 b.Tesing the backend

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,30 +1,31 @@
+const mongoose = require("mongoose");
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
 // route for getting all the notes
-notesRouter.get("/", (request, response) => {
-  Note.find({}).then((notes) => response.json(notes));
+notesRouter.get("/", async (request, response) => {
+  const notes = await Note.find({});
+  response.json(notes);
 });
 //route for fetching a single resource
-notesRouter.get("/:id", (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
+notesRouter.get("/:id", async (request, response) => {
+  const id = request.params.id;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return response.status(400).json({ error: "invalid id" });
+  }
+  const note = await Note.findById(id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
 });
-notesRouter.delete("/api/notes/:id", (request, response) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
+notesRouter.delete("/:id", async (request, response) => {
+  await Note.findByIdAndDelete(request.params.id);
+  response.status(204).end();
 });
 //route for adding data (new note)
-notesRouter.post("/", (request, response, next) => {
+notesRouter.post("/", async (request, response) => {
   const body = request.body;
   if (!body.content) {
     return response.status(400).json({ error: "content missing" });
@@ -34,12 +35,8 @@ notesRouter.post("/", (request, response, next) => {
     important: body.important || false,
   });
 
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote);
-    })
-    .catch((error) => next(error));
+  const savedNote = await note.save();
+  response.status(201).json(savedNote);
 });
 
 notesRouter.put("/:id", (request, response, next) => {
@@ -56,7 +53,6 @@ notesRouter.put("/:id", (request, response, next) => {
         response.json(updatedNote);
       })
       .catch((error) => {
-        // console.log(error.response.data.error);
         next(error);
       });
   });
