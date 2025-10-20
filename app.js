57cc60b1a77aac8fb855e1dd8d711411e837bb5b
Lidet Voeun <lidetvoeun@gmail.com>
2025-10-20 14:21:41 +0700

    End of Part 4

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const config = require("./utils/config");
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
 const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 const app = express();
 logger.info("connecting to ", config.MONGODB_URI);
@@ -20,6 +22,8 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,3 @@
-// const info = (...params) => {
-//   console.log(...params);
-// };
-// const error = (...params) => {
-//   console.error(...params);
-// };
 const info = (...params) => {
   if (process.env.NODE_ENV !== "test") {
     console.log(...params);
